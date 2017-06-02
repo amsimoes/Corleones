d1e58f5122779d863fbc52a0391a1ade8f36c8e6
António Simões <asimoes@student.dei.uc.pt>
2017-06-02 16:41:01 +0100

    B 55 pts

--- code_gen.c
+++ code_gen.c
@@ -8,6 +8,7 @@ char** global_strings;
 int temp_count = 1;
 int string_count = 0;
 int method_index = 0;
+int array_counter = 0;
 
 int is_global(char* var_name, char* var_type) {
 	if (var_name != NULL && var_type != NULL) {
@@ -46,6 +47,12 @@ char* get_llvm_type(char* node_type) {
 	return "undefined";
 }
 
+void set_node_llvm_address(node_t* n) {
+	char aux[32];
+	sprintf(aux, "%d", temp_count);
+	n->llvm_address = (char*) strdup(aux);
+}
+
 char* strip_underscores(char* declit) {
 	char* i = declit;
 	char* j = declit;
@@ -69,14 +76,41 @@ void code_gen_minus(node_t* n_minus) {
 }
 
 void code_gen_assign(node_t* n) {
-	char* var_type = (char*) strdup(get_llvm_type(n->data_type));
-	if (!strcmp(var_type, "i32")) {
-		printf("  store %s %d, %s* %%%s\n", var_type, atoi(strip_underscores((char*)n->children[1]->value)), var_type, n->children[0]->value);
-	} else if (!strcmp(var_type, "double")) {
-		printf("  store %s %.16f, %s* %%%s\n", var_type, atof(n->children[1]->value), var_type, n->children[0]->value);
-	} else if (!strcmp(var_type, "i1")) {
-		printf("  store %s %s, %s* %%%s\n", var_type, n->children[1]->value, var_type, n->children[0]->value);
+	if (!strcmp(n->children[0]->data_type, n->children[1]->data_type)) {
+		char* var_type = (char*) strdup(get_llvm_type(n->data_type));
+
+		//printf("n->children[1]->type = %s / n->children[1]->value = %s\n", n->children[1]->type, n->children[1]->value);
+		if (!is_global(n->children[0]->value, n->children[0]->data_type)) {
+			if (!strcmp(var_type, "i32")) {
+				printf("  store %s %%%d, %s* %%%s\n", var_type, temp_count-1, var_type, n->children[0]->value);
+			} else if (!strcmp(var_type, "double")) {
+				printf("  store %s %%%d, %s* %%%s\n", var_type, temp_count-1, var_type, n->children[0]->value);
+			} else if (!strcmp(var_type, "i1")) {
+				printf("  store %s %%%d, %s* %%%s\n", var_type, temp_count-1, var_type, n->children[0]->value);
+			}
+		} else {
+			if (!strcmp(var_type, "i32")) {
+				printf("  store %s %%%d, %s* @global.%s\n", var_type, temp_count-1, var_type, n->children[0]->value);
+			} else if (!strcmp(var_type, "double")) {
+				printf("  store %s %%%d, %s* @global.%s\n", var_type, temp_count-1, var_type, n->children[0]->value);
+			} else if (!strcmp(var_type, "i1")) {
+				printf("  store %s %%%d, %s* @global.%s\n", var_type, temp_count-1, var_type, n->children[0]->value);
+			}
+		}
+
+	} else {
+		printf("  %%%d = sitofp i32 %%%d to double\n", temp_count, temp_count-1);
+		temp_count++;
+
+		printf("  store double %%%d, double", temp_count-1);
+		if (is_global(n->children[0]->value, n->children[0]->data_type)) {
+			printf("* @global.%s\n", n->children[0]->value);
+		} else {
+			printf("* %%%s\n", n->children[0]->value);
+		}
 	}
+
+	//code_gen(n->children[0]);
 }
 
 void code_gen_vardecl(node_t* n) {
@@ -93,16 +127,10 @@ void code_gen_return(node_t* ret) {
 	}
 }
 
-void set_node_llvm_address(node_t* n) {
-	char aux[32];
-	sprintf(aux, "%d", temp_count);
-	n->llvm_address = (char*) strdup(aux);
-}
-
 void code_gen_id(node_t* n_id) {
 	char* llvm_type = (char*) strdup(get_llvm_type(n_id->data_type));
 	if (is_global(n_id->value, n_id->data_type))
-		printf("  %%%d = load %s, %s* @%s\n", temp_count, llvm_type, llvm_type, n_id->value);
+		printf("  %%%d = load %s, %s* @global.%s\n", temp_count, llvm_type, llvm_type, n_id->value);
 	else
 		printf("  %%%d = load %s, %s* %%%s\n", temp_count, llvm_type, llvm_type, n_id->value);
 	set_node_llvm_address(n_id);
@@ -116,7 +144,7 @@ void code_gen_declit(node_t* n_declit) {
 }
 
 void code_gen_reallit(node_t* n_reallit) {
-	printf("  %%%d = fadd double 0.0, %.16f\n", temp_count, atof(n_reallit->value));
+	printf("  %%%d = fadd double 0.0, %.16E\n", temp_count, atof(strip_underscores(n_reallit->value)));
 	set_node_llvm_address(n_reallit);
 	temp_count++;
 }
@@ -126,6 +154,7 @@ void code_gen_boollit(node_t* n_boollit) {
 	set_node_llvm_address(n_boollit);
 	temp_count++;
 }
+
 
 void define_print_boolean() {
 	printf("define void @print_boolean(i1 %%a) {");
@@ -145,10 +174,10 @@ void define_print_boolean() {
 }
 
 void escape_string(char* strlit) {
-	char *temp = (char*) malloc (4096);
+	char* temp = (char*) calloc (1, sizeof(1024) * sizeof(char));
 	int i, j, start = 0, escapes = 0;
 
-	for (i=0, j=0; strlit[i]; i++) {
+	for (i=0, j=0; strlit[i] != 0; i++) {
 		if (strlit[i] == '\\') {
 			escapes -= 2;
 			i++;
@@ -189,49 +218,59 @@ void escape_string(char* strlit) {
 				temp[j++] = strlit[i];
 				escapes -= 4;
 
-				global_strings[string_count] = (char*) malloc (strlen(temp) + 1);
-				//tobeprint[n_tobeprint] = (char*) malloc (strlen(temp ) + 60);
+				global_strings[string_count] = (char*) malloc (j + 60);
 
-				tempvar++;
 				printf("  %%%d = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([%d x i8], [%d x i8]* @.str.%d, i32 0, i32 0))\n", temp_count, \
-					(int) strlen(temp) + escapes, (int) strlen(temp) + escapes, string_count+1);
+					j + escapes, j + escapes, string_count+1);
 
-				sprintf(global_strings[string_count] ,"@.str.%d = private unnamed_addr constant [%d x i8] c%s", string_count+1, (int) strlen(temp) + escapes, temp);
+				//strcpy(global_strings[string_count], temp);
+				snprintf(global_strings[string_count], j+60, "@.str.%d = private unnamed_addr constant [%d x i8] c%s", string_count+1, j + escapes, temp);
 				
+				free(temp);
+				free(strlit);
+				temp_count++;
 				string_count++;
+				break;
 			}
 		
 		} else if (strlit[i] == '%') {
 				temp[j++] = '%';
 				temp[j++] = '%';
 		} else {
-			temp[j++] = strlit[i];
+			temp[j] = strlit[i]; j++;
 		}
 	}
 }
 
-void code_gen_print(node_t* print) {
+void code_gen_print(node_t* print) {	
+	//printf("print->children[0]->data_type = %s / print->children[1]->value = %s\n", print->children[0]->data_type, print->children[0]->value);
+
 	if (!strcmp(print->children[0]->data_type, "int")) {
-		printf("  %%%d = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.str.int, i32 0, i32 0), i32 %%%d)\n", temp_count, atoi(print->children[0]->llvm_address));	
+		printf("  %%%d = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.str.int, i32 0, i32 0), i32 %%%d)\n", temp_count, temp_count-1);
+		//printf("  %%%d = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.str.int, i32 0, i32 0), i32 %%%d)\n", temp_count, atoi(print->children[0]->llvm_address));	
 		temp_count++;
 	} else if (!strcmp(print->children[0]->data_type, "double")) {
-		printf("  %%%d = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([7 x i8], [7 x i8]* @.str.double, i32 0, i32 0), double %%%d)\n", temp_count, atoi(print->children[0]->llvm_address));	
+		printf("  %%%d = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([7 x i8], [7 x i8]* @.str.double, i32 0, i32 0), double %%%d)\n", temp_count, temp_count-1);
+		//printf("  %%%d = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([7 x i8], [7 x i8]* @.str.double, i32 0, i32 0), double %%%d)\n", temp_count, atoi(print->children[0]->llvm_address));	
 		temp_count++;
 	} else if (!strcmp(print->children[0]->data_type, "boolean")) {
-		printf("  call void @print_boolean(i1 %%%s)\n", print->children[0]->llvm_address);
+		printf("  call void @print_boolean(i1 %%%d)\n", temp_count-1);
+		//printf("  call void @print_boolean(i1 %%%s)\n", print->children[0]->llvm_address);
 	} else if (!strcmp(print->children[0]->data_type, "String")) {
-		escape_string(print->children[0]->value);
+		char* aux = (char*) strdup(print->children[0]->value);
+		escape_string(aux);
 	}
 }
 
 void code_gen_field_decl(node_t* n) {
 	if (!strcmp(n->children[0]->type, "Int")) {
-		printf("@%s = common global i32 0\n", n->children[1]->value);
+		printf("@global.%s = common global i32 0\n", n->children[1]->value);
 	} else if (!strcmp(n->children[0]->type, "Double")) {
-		printf("@%s = common global double %.16f\n", n->children[1]->value, 0.0);
+		printf("@global.%s = common global double %.16E\n", n->children[1]->value, 0.0);
 	} else if (!strcmp(n->children[0]->type, "Bool")) {
-		printf("@%s = common global i1 0\n", n->children[1]->value);
+		printf("@global.%s = common global i1 0\n", n->children[1]->value);
 	}	
+	printf("\n");
 }
 
 void code_gen_method_params(char* method_name, node_t* method_params) {
@@ -253,16 +292,17 @@ void code_gen_method_params(char* method_name, node_t* method_params) {
 		printf("(i32 %%argc, i8** %%argv) {\n");
 		if (!strcmp(return_type, "int")) {
 			printf("  %%%d = alloca %s\n", temp_count, return_type);
+			printf("  store i32 0, i32* %%1\n");
 			temp_count++;
 		}
-		printf("  %%%d = alloca i32\n", temp_count);
-		temp_count++;
-		printf("  %%%d = alloca i8**\n", temp_count);
-		temp_count++;
-		if (!strcmp(return_type, "int"))
-			printf("  store i32 0, i32* %%1\n");
-		printf("  store i32 %%argc, i32* %%2\n");
-		printf("  store i8** %%argv, i8*** %%3")
+
+        printf("  %%argc.addr = alloca i32\n");
+		printf("  %%argv.addr = alloca i8**\n");
+		printf("  store i32 %%argc, i32* %%argc.addr\n");
+		printf("  store i8** %%argv, i8*** %%argv.addr\n");
+		printf("  %%length = alloca i32\n");
+		//printf("  store i32 %%argc.addr, i32* %%length\n");
+			
 		return;
 	}
 
@@ -286,7 +326,21 @@ void code_gen_method_header(node_t* method_header) {
 	code_gen_method_params(method_name, method_header->children[2]);
 }
 
+void code_gen_global_strings() {
+	int i;
+	for (i=0; i < string_count; i++) {
+		printf("%s\n", global_strings[i]);
+	}
+
+	printf("\n@.str.true = private unnamed_addr constant [6 x i8] c\"true\\0A\\00\"\n");
+	printf("@.str.false = private unnamed_addr constant [7 x i8] c\"false\\0A\\00\"\n");
+	printf("@.str.int = private unnamed_addr constant [4 x i8] c\"%%d\\0A\\00\"\n");
+	printf("@.str.double = private unnamed_addr constant [7 x i8] c\"%%.16E\\0A\\00\"\n\n");
+}
+
 void code_gen(node_t* n) {
+	//printf("n->type = %s / n->value = %s\n", n->type, n->value);
+
 	int i;
 	if (!strcmp(n->type, "Program")) {
 		class_name = (char*) strdup(table[0]->name);
@@ -294,19 +348,9 @@ void code_gen(node_t* n) {
 		global_strings = (char**) malloc (sizeof(char*) * 4096);
 
 		printf("define i32 @main(i32 %%argc, i8** %%argv) {\n");
-		
 		printf("  call void @main.string(i32 %%argc, i8** %%argv)\n");
 		printf("  ret i32 0\n");
 		printf("}\n\n");
-
-		printf("\n@.str.true = private unnamed_addr constant [6 x i8] c\"true\\0A\\00\"\n");
-		printf("@.str.false = private unnamed_addr constant [7 x i8] c\"false\\0A\\00\"\n");
-		printf("@.str.int = private unnamed_addr constant [4 x i8] c\"%%d\\0A\\00\"\n");
-		printf("@.str.double = private unnamed_addr constant [7 x i8] c\"%%.16E\\0A\\00\"\n\n");
-		
-		printf("declare i32 @printf(i8*, ...)\n\n");
-		printf("declare i32 @atoi(...)\n\n");
-		define_print_boolean();
 
 		for (i=0; i < n->n_children; i++) {
 			if (!strcmp(n->children[i]->type, "FieldDecl"))
@@ -318,14 +362,17 @@ void code_gen(node_t* n) {
 				code_gen(n->children[i]);
 		}
 
-		for (i=0; i < string_count; i++) {
-			printf("%s\n", global_strings[i]);
-		}
+		code_gen_global_strings();
+		
+		printf("declare i32 @printf(i8*, ...)\n\n");
+		printf("declare i32 @atoi(...)\n\n");
+		define_print_boolean();
 	} else if (!strcmp(n->type, "FieldDecl")) {
 		code_gen_field_decl(n);
 	} else if (!strcmp(n->type, "MethodDecl")) {
 		method_index++;
 		temp_count = 1;
+		array_counter = 0;
 		
 		code_gen_method_header(n->children[0]);
 		for (i=0; i < n->children[1]->n_children; i++) {
@@ -340,7 +387,7 @@ void code_gen(node_t* n) {
 		code_gen(n->children[0]);
 		code_gen_print(n);
 	} else if (!strcmp(n->type, "Length")) {
-		code_gen_length(n);
+		//code_gen_length(n);
 	} else if (!strcmp(n->type, "Id")) {
 		code_gen_id(n);
 	} else if (!strcmp(n->type, "DecLit")) {
@@ -350,13 +397,26 @@ void code_gen(node_t* n) {
 	} else if (!strcmp(n->type, "BoolLit")) {
 		code_gen_boollit(n);
 	} else if (!strcmp(n->type, "StrLit")) {
-
+		//code_gen_strlit(n);
 	} else if (!strcmp(n->type, "Assign")) {
+		code_gen(n->children[1]);
 		code_gen_assign(n);
 	} else if (!strcmp(n->type, "Minus")) {
-		code_gen_unary(n);
 		code_gen(n->children[0]);
+		code_gen_minus(n);
 	} else if (!strcmp(n->type, "Plus")) {
 		code_gen(n->children[0]);
+	} else if (!strcmp(n->type, "ParseArgs")) {
+		code_gen(n->children[1]);
+		printf("  %%%d = add i32 %%%d, 1\n", temp_count, temp_count-1);
+		array_counter++;
+		temp_count++;
+		printf("  %%%d = load i8**, i8*** %%argv.addr\n", temp_count);
+		temp_count++;
+		printf("  %%arraycount%d = getelementptr inbounds i8*, i8** %%%d, i32 %%%d\n", array_counter, temp_count-1, temp_count-2);
+		printf("  %%%d = load i8*, i8** %%arraycount%d\n", temp_count, array_counter);
+		temp_count++;
+		printf("  %%%d = call i32 (i8*, ...) bitcast (i32 (...)* @atoi to i32 (i8*, ...)*)(i8* %%%d)\n",temp_count, temp_count-1);
+		temp_count++;
 	}
 }
--- lul.ll
+++ lul.ll
@@ -1,32 +1,55 @@
-; ModuleID = 'lul.c'
-target datalayout = "e-m:e-i64:64-f80:128-n8:16:32:64-S128"
-target triple = "x86_64-unknown-linux-gnu"
-
-@.str = private unnamed_addr constant [7 x i8] c"%s %s\0A\00", align 1
-
-; Function Attrs: nounwind uwtable
-define i32 @main(i32 %argc, i8** %argv) #0 {
-  %1 = alloca i32, align 4
-  %2 = alloca i32, align 4
-  %3 = alloca i8**, align 8
-  store i32 0, i32* %1, align 4
-  store i32 %argc, i32* %2, align 4
-  store i8** %argv, i8*** %3, align 8
-  %4 = load i8**, i8*** %3, align 8
-  %5 = getelementptr inbounds i8*, i8** %4, i64 0
-  %6 = load i8*, i8** %5, align 8
-  %7 = load i8**, i8*** %3, align 8
-  %8 = getelementptr inbounds i8*, i8** %7, i64 1
-  %9 = load i8*, i8** %8, align 8
-  %10 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([7 x i8], [7 x i8]* @.str, i32 0, i32 0), i8* %6, i8* %9)
-  ret i32 9
+define i32 @main(i32 %argc, i8** %argv) {
+  call void @main.string(i32 %argc, i8** %argv)
+  ret i32 0
 }
 
-declare i32 @printf(i8*, ...) #1
+@global.a = common global double 0.0000000000000000E+00
+@global.b = common global i1 0
+@global.k = common global i32 0
 
-attributes #0 = { nounwind uwtable "disable-tail-calls"="false" "less-precise-fpmad"="false" "no-frame-pointer-elim"="true" "no-frame-pointer-elim-non-leaf" "no-infs-fp-math"="false" "no-nans-fp-math"="false" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+fxsr,+mmx,+sse,+sse2" "unsafe-fp-math"="false" "use-soft-float"="false" }
-attributes #1 = { "disable-tail-calls"="false" "less-precise-fpmad"="false" "no-frame-pointer-elim"="true" "no-frame-pointer-elim-non-leaf" "no-infs-fp-math"="false" "no-nans-fp-math"="false" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+fxsr,+mmx,+sse,+sse2" "unsafe-fp-math"="false" "use-soft-float"="false" }
+define void @main.int(i32 %a) {
+  %main.a = alloca i32
+  %1 = load i32, i32* @global.k
+  %2 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.str.int, i32 0, i32 0), i32 %1)
+  %3 = add i1 0, true
+  store i1 %3, i1* @global.b
+  %4 = add i32 0, 433433
+  %5 = sub i32 -0, %4
+  %6 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.str.int, i32 0, i32 0), i32 %5)
+  %main.k = alloca double
+  %7 = fadd double 0.0, 3.5000000000000000E+00
+  store double %7, double* %main.k
+  %8 = load double, double* %main.k
+  %9 = fsub double -0.0, %8
+  %10 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([7 x i8], [7 x i8]* @.str.double, i32 0, i32 0), double %9)
+  %11 = load i1, i1* @global.b
+  call void @print_boolean(i1 %11)
+  ret void
+}
 
-!llvm.ident = !{!0}
 
-!0 = !{!"clang version 3.8.1 (tags/RELEASE_381/final)"}
+@.str.true = private unnamed_addr constant [6 x i8] c"true\0A\00"
+@.str.false = private unnamed_addr constant [7 x i8] c"false\0A\00"
+@.str.int = private unnamed_addr constant [4 x i8] c"%d\0A\00"
+@.str.double = private unnamed_addr constant [7 x i8] c"%.16E\0A\00"
+
+declare i32 @printf(i8*, ...)
+
+declare i32 @atoi(...)
+
+define void @print_boolean(i1 %a) {
+  %1 = alloca i1
+  store i1 %a, i1* %1
+  %2 = load i1, i1* %1
+  %3 = icmp eq i1 %2, 0
+  br i1 %3, label %false, label %true
+  false:
+  %4 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([7 x i8], [7 x i8]* @.str.false, i32 0, i32 0))
+  br label %ret
+  true:
+  %5 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([6 x i8], [6 x i8]* @.str.true, i32 0, i32 0))
+  br label %ret
+  ret:
+  ret void
+}
+
